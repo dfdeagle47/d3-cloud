84587e81bf7fa590fc1a7f1c71be24614e8d1862
Jonathan Alzetta <jonathan@wooclap.com>
2017-04-17 17:52:54 +0200

    [UP] build

--- build/d3.layout.cloud.js
+++ build/d3.layout.cloud.js
@@ -120,8 +120,6 @@ module.exports = function() {
       tag.x = startX + dx;
       tag.y = startY + dy;
 
-      if (tag.x + tag.x0 < 0 || tag.y + tag.y0 < 0 ||
-          tag.x + tag.x1 > size[0] || tag.y + tag.y1 > size[1]) continue;
       // TODO only check for collisions within current bounds.
       if (!bounds || !cloudCollide(tag, board, size[0])) {
         if (!bounds || collideRects(tag, bounds)) {
